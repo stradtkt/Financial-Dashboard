cc5016a81c731c86b6c80c5d6bf4990d65e3d02a
Kevin <stradtkt22@gmail.com>
2019-03-01 01:01:24 -0500

    InitialCommit

---
+++ Models/DashboardContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dashboard.Models
+{
+    public class DashboardContext : DbContext
+    {
+        public DashboardContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Expenses> Expenses { get; set; }
+    }
+}
