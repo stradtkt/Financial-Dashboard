ca4bcd5216e47b07bd9a3d49ce2d806b72bea039
Kevin <stradtkt22@gmail.com>
2019-03-01 01:40:23 -0500

    Added User and Expense

---
+++ Models/Expenses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dashboard.Models
+{
+    public class Expenses
+    {
+        [Key]
+        public int ExpenseId { get; set; }
+        public String Item { get; set; }
+        public decimal Cost { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dashboard.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
